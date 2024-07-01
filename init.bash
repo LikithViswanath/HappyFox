522ab383964e8b00d8eb8603f06408f5c3c3ce2a
likith <likithvishwanath6@gmail.com>
2024-07-01 17:54:25 +0530

    stable 3.5

--- init.bash
+++ init.bash
@@ -13,11 +13,7 @@ source virtual-env/bin/activate || { echo "Failed to activate virtualenv. Exitin
 echo "** Installing requirements from requirements.txt..."
 pip3 install -r requirements.txt || { echo "Failed to install requirements. Exiting..."; deactivate; exit 1; }
 
-# 3. Setup python path (assuming current directory is project root)
-echo "** Setting up Python path..."
-export PYTHONPATH=$PYTHONPATH:./
-
-# 4. Start database using docker-compose
+# 3. Start database using docker-compose
 echo "** Starting database containers..."
 docker-compose -f docker-compose.yaml up -d || { echo "Failed to start database containers. Exiting..."; deactivate; exit 1; }
 
