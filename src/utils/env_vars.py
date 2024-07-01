3d3154c450e70e397f82f710f1588e89754bba9d
likith <likithvishwanath6@gmail.com>
2024-07-01 14:57:42 +0530

    stable 3.3

--- src/utils/env_vars.py
+++ src/utils/env_vars.py
@@ -8,8 +8,8 @@ DATA_BASE_URL = f'mysql+mysqlconnector://{user}:{password}@{host}:3306/{DATA_BAS
 SQL_ENGINE = create_engine(DATA_BASE_URL)
 
 SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
-CREDENTIALS_FILE = 'src/credentials.json'
-TOKEN_FILE = 'src/token.json'
+CREDENTIALS_FILE = 'credentials.json'
+TOKEN_FILE = 'token.json'
 RULES_FILE = 'src/config/rules.json'
 USER_ID = "magnetoxmen6@gmail.com"
 
