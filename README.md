aa75f94d16e74d774eea0ec74678cba267246ff0
likith <likithvishwanath6@gmail.com>
2024-07-01 14:38:10 +0530

    stable 3.3

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 1. Run the init.bash file which takes care of creating venv and starting docker compose
     ```{console}
-   bash init.bash
+   bash HappyFox/init.bash
    ```
 3. Run the mail_data_loader.py script to load data from gmail to DB.
     ```{console}
-    python3 src/scripts/gmail_controller.py --flush True --limit 12
+    python3 HappyFox/src/scripts/gmail_controller.py --flush True --limit 12
     ```
 4. Run the rule_executor.py script to perform required actions.
     ```{console}
-    python3 src/scripts/rule_action_controller.py --test_path src//rules.json
+    python3 HappyFox/src/scripts/rule_action_controller.py --test_path src//rules.json
     ```**
--- init.bash
+++ init.bash
@@ -8,7 +8,7 @@ source HappyFox/bin/activate || { echo "Failed to activate virtualenv. Exiting..
 
 # 2. Install requirements
 echo "** Installing requirements from requirements.txt..."
-pip3 install -r requirements.txt || { echo "Failed to install requirements. Exiting..."; deactivate; exit 1; }
+pip3 install -r HappyFox/requirements.txt || { echo "Failed to install requirements. Exiting..."; deactivate; exit 1; }
 
 # 3. Setup python path (assuming current directory is project root)
 echo "** Setting up Python path..."
@@ -16,7 +16,7 @@ export PYTHONPATH=$PYTHONPATH:./
 
 # 4. Start database using docker-compose
 echo "** Starting database containers..."
-docker-compose -f dockerfiles/docker-compose.yaml up -d || { echo "Failed to start database containers. Exiting..."; deactivate; exit 1; }
+docker-compose -f HappyFox/docker-compose.yaml up -d || { echo "Failed to start database containers. Exiting..."; deactivate; exit 1; }
 
 echo "** Script ($SCRIPT_NAME) completed successfully! **"
 
